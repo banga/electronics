b453353cf2ffe76291fbbf9c68000982ef882db3
Shrey Banga <shrey@quip.com>
2017-06-24 10:01:49 -0700

    Check-in code for rgb-led-display

---
+++ rgb-led-display/draw_image.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+
+import Image
+import ImageDraw
+import sys
+import time
+from rgbmatrix import Adafruit_RGBmatrix
+
+matrix = Adafruit_RGBmatrix(32, 1)
+
+
+def draw_animated(filename):
+    image = Image.open(filename)
+    duration = image.info.get("duration", 30) / 1000.0
+    while True:
+        image.load()          # Must do this before SetImage() calls
+        matrix.SetImage(image.im.id, 0, 0)
+        time.sleep(duration)
+        try:
+            image.seek(image.tell() + 1)
+        except:
+            matrix.Clear()
+            return
+
+
+def draw_static(filename):
+    image = Image.open(filename)
+    image.load()          # Must do this before SetImage() calls
+    matrix.Clear()
+    matrix.SetImage(image.im.id, 0, 0)
+    raw_input()
+
+
+for filename in sys.argv[1:]:
+    print filename
+    draw_animated(filename)
---
+++ rgb-led-display/spiral.py
@@ -0,0 +1,55 @@
+import time
+from rgbmatrix import Adafruit_RGBmatrix
+
+matrix = Adafruit_RGBmatrix(32, 1)
+matrix.Clear()
+
+
+def spiral(n):
+    center = (n - 1) / 2
+
+    position = center * (1 + 1j)
+    direction = 1
+    arm_length = 1
+    count = 0
+
+    while True:
+        for _ in range(2):
+            for _ in range(2):
+                for _ in range(arm_length):
+                    coords = int(position.real), int(position.imag)
+                    yield coords
+                    position += direction
+                    count += 1
+                    if count == n * n:
+                        return
+                direction *= 1j
+            arm_length += 1
+
+
+def main():
+    trail = []
+    trail_size = 32
+
+    def display(point):
+        trail.append(point)
+        while len(trail) > trail_size:
+            trail.pop(0)
+        # matrix.Clear()
+        for i, (x, y) in enumerate(trail):
+            fade = 0.2 + 0.8 * (1 + i) * 1.0 / trail_size
+            matrix.SetPixel(x, y,
+                int(100 * fade),
+                int(225 * fade),
+                int(255 * fade)
+            )
+        time.sleep(0.005)
+
+    for point in spiral(32):
+        display(point)
+
+    time.sleep(2)
+    matrix.Clear()
+
+
+main()
